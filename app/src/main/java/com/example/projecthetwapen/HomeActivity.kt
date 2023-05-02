a67e98fc8755c2e5bc514116068f82747b64206b
Erizon <dragonwarrior66.rr@gmail.com>
2023-05-02 15:43:28 +0200

    Error with navController
    kan de findNavController niet werkend krijgen

--- app/src/main/java/com/example/projecthetwapen/HomeActivity.kt
+++ app/src/main/java/com/example/projecthetwapen/HomeActivity.kt
@@ -2,10 +2,32 @@ package com.example.projecthetwapen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+//import com.example.projecthetwapen.binding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val navView : BottomNavigationView = findViewById(R.id.menubar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_subscription
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
 }
