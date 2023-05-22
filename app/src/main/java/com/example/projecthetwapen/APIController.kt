a563d0698f29e438a68feddc6a2f554a2eadc695
Erizon <dragonwarrior66.rr@gmail.com>
2023-05-22 10:57:35 +0200

    api small change

--- app/src/main/java/com/example/projecthetwapen/APIController.kt
+++ app/src/main/java/com/example/projecthetwapen/APIController.kt
@@ -6,15 +6,18 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 
 
 class APIController (var context: Context) {
     // stap 1 url naar de API in een var string gezet
-    var url: String = "https://hetwapen.projects.adainforma.tk/api/v1"
     //stap 2 inladen volley library file -> project structure -> dependencies -> + -> library dependecies -> typ in : com.android.volley -> search
     //stap 3 RequestQueue kan een verzoek indienen bij de API
-    var queue: RequestQueue = Volley.newRequestQueue(context)
     // stap 4 maak functie aan
+
+    var url: String = "https://hetwapen.projects.adainforma.tk/api/v1"
+    var queue: RequestQueue = Volley.newRequestQueue(context)
+    val gson : Gson = Gson()
 
     fun getData(){
         //stap 5
