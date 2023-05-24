0da1440adb93e158357ab129ae7b61e252103b71
Erizon <dragonwarrior66.rr@gmail.com>
2023-05-24 11:17:35 +0200

    small edits

--- app/src/main/java/com/example/projecthetwapen/UI/detail_beer/DetailPaginaFragment.kt
+++ app/src/main/java/com/example/projecthetwapen/UI/detail_beer/DetailPaginaFragment.kt
@@ -1,10 +1,11 @@
-package com.example.projecthetwapen
+package com.example.projecthetwapen.UI.detail_beer
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.projecthetwapen.R
 
 
 class detailPaginaFragment : Fragment() {
